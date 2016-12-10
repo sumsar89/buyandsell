04ae0de92a05726f5413e1e804a2ff1f34230c39
gregdunne89 <gregdunne89@hotmail.com>
2016-12-10 10:12:41 -0500

    final

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 //code sourced from git+https://github.com/mikhail-nci/wad-simple-demo.git with enhancements by GregDunne
 
 var http = require('http'),
-    path = require('path'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    fs = require('fs'),
-    js2xmlparser = require('js2xmlparser'),
-    libxslt = require('libxslt');
+  path = require('path'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  fs = require('fs'),
+  js2xmlparser = require('js2xmlparser'),
+  libxslt = require('libxslt');
 
 var router = express();
 var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'views')));
-router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 router.use(bodyParser.json());
 
 // GET request to dislay index.html located inside /views folder
@@ -22,22 +24,24 @@ router.get('/', function(req, res) {
 
 // HTML produced by XSL Transformation
 router.get('/get/html', function(req, res) {
-  
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    
-    var docSource = fs.readFileSync('Saleboard.xml', 'utf8');
-    var stylesheetSource = fs.readFileSync('Saleboard.xsl', 'utf8');
-    
-    var doc = libxslt.libxmljs.parseXml(docSource);
-    var stylesheet = libxslt.parse(stylesheetSource);
-    
-    var result = stylesheet.apply(doc);
-    
-    res.end(result.toString());
-  
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  var docSource = fs.readFileSync('Saleboard.xml', 'utf8');
+  var stylesheetSource = fs.readFileSync('Saleboard.xsl', 'utf8');
+
+  var doc = libxslt.libxmljs.parseXml(docSource);
+  var stylesheet = libxslt.parse(stylesheetSource);
+
+  var result = stylesheet.apply(doc);
+
+  res.end(result.toString());
+
 });
 
-//Code for For Sale Input
+//Code for 'For Sale' Input
 // POST request to add to JSON & XML files
 router.post('/post/jsonSale', function(req, res) {
 
@@ -46,13 +50,11 @@ router.post('/post/jsonSale', function(req, res) {
 
     // Read in a JSON file
     var JSONfile = fs.readFileSync('Saleboard.json', 'utf8');
-    //JSONfile.ad.pop(obj); //didn't work either
 
     // Parse the JSON file in order to be able to edit it 
     var JSONparsed = JSON.parse(JSONfile);
 
-    // Add a new record into ad array within the JSON file   
-    //JSONparsed.sale.shift(obj); //replaces 1st item of json array when adding new posting
+    // Add a new record into the sale array within the JSON file   
     JSONparsed.sale.push(obj);
 
     // Beautify the resulting JSON file
@@ -71,7 +73,7 @@ router.post('/post/jsonSale', function(req, res) {
 
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
-  
+
   // Re-direct the browser back to the page, where the POST request came from
   res.redirect('back');
 
@@ -93,8 +95,7 @@ router.post('/post/json', function(req, res) {
     // Parse the JSON file in order to be able to edit it 
     var JSONparsed = JSON.parse(JSONfile);
 
-    // Add a new record into ad array within the JSON file  
-    //JSONparsed.ad = {}; - Doesn't work
+    // Add a new record into the buy array within the JSON file  
     JSONparsed.buy.push(obj);
 
     // Beautify the resulting JSON file
@@ -113,7 +114,7 @@ router.post('/post/json', function(req, res) {
 
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
-  
+
   // Re-direct the browser back to the page, where the POST request came from
   res.redirect('back');
 
@@ -121,7 +122,6 @@ router.post('/post/json', function(req, res) {
 
 
 //Code for deleting Sale record
-// POST request to add to JSON & XML files
 router.post('/delete/json', function(req, res) {
 
   // Function to read in a JSON file, add to it & convert to XML
@@ -133,8 +133,7 @@ router.post('/delete/json', function(req, res) {
     // Parse the JSON file in order to be able to edit it 
     var JSONparsed = JSON.parse(JSONfile);
 
-    // Add a new record into ad array within the JSON file  
-    //JSONparsed.ad = {}; - Doesn't work
+    // Deletes the last record from the Sale array/table
     JSONparsed.sale.pop(obj);
 
     // Beautify the resulting JSON file
@@ -153,14 +152,13 @@ router.post('/delete/json', function(req, res) {
 
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
-  
+
   // Re-direct the browser back to the page, where the POST request came from
   res.redirect('back');
 
 });
 
 //Code for deleting Buy record
-// POST request to add to JSON & XML files
 router.post('/delete/jsonBuy', function(req, res) {
 
   // Function to read in a JSON file, add to it & convert to XML
@@ -172,8 +170,7 @@ router.post('/delete/jsonBuy', function(req, res) {
     // Parse the JSON file in order to be able to edit it 
     var JSONparsed = JSON.parse(JSONfile);
 
-    // Add a new record into ad array within the JSON file  
-    //JSONparsed.ad = {}; - Doesn't work
+    // Deletes the last record from the buy array/table
     JSONparsed.buy.pop(obj);
 
     // Beautify the resulting JSON file
@@ -192,7 +189,7 @@ router.post('/delete/jsonBuy', function(req, res) {
 
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
-  
+
   // Re-direct the browser back to the page, where the POST request came from
   res.redirect('back');
 
